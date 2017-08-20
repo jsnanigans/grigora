fe63490377b0b452071f7b4e42d4d99c96e04744
jsnanigans <megamaaaan@gmail.com>
2017-08-20 10:53:30 +0200

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+module.exports = {
+  options: {
+    fileEnding: 'html',
+    beforeEach: {
+      // components: ['head']
+    },
+    afterEach: {
+      // components: ['footer']
+    }
+  },
+  pages: [
+    {
+      name: 'index',
+      components: ['image']
+    }
+  ]
+}
