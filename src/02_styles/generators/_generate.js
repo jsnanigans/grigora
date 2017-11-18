c903ad95266ff84b88b8bf02d9e945623e6e6a09
qwe <megamaaaan@gmail.com>
2017-11-18 23:10:58 +0100

    Generate classes only when required - and when build starts

---
+++ src/02_styles/generators/_generate.js
@@ -0,0 +1,66 @@
+const fs = require('fs')
+const path = require('path')
+const stylus = require('stylus')
+
+const sourceFiles = [
+  path.join(__dirname, 'grid.styl'),
+  path.join(__dirname, 'positioning.styl'),
+  path.join(__dirname, 'spacing.styl'),
+  path.join(__dirname, '../../01_variables/layout.styl')
+]
+
+const outFile = path.join(__dirname, '_generated.css')
+
+const determinUpdateRequired = _ => {
+  if (!fs.existsSync(outFile)) {
+    return true
+  }
+
+  let latestSourceChange = 0
+  sourceFiles.forEach(file => {
+    const change = fs.existsSync(file) ? fs.statSync(file).mtimeMs : 0
+
+    if (change > latestSourceChange) {
+      latestSourceChange = change
+    }
+  })
+
+  const lastGenerated = fs.statSync(outFile).mtimeMs
+
+  if (lastGenerated < latestSourceChange) {
+    return true
+  }
+
+  return false
+}
+
+const generateFile = _ => {
+  const genPath = path.join(__dirname, '_gen')
+  const varPath = path.join(__dirname, '../../01_variables/index')
+  stylus(`@import '${varPath}' \n @import '${genPath}'`)
+    .use(require('rupture')())
+    .use(require('jeet')())
+    .render((err, css) => {
+      if (err) {
+        console.error(err)
+        return err
+      }
+
+      fs.writeFileSync(outFile, css, err => {
+        if (err) {
+          console.error(err)
+          return err
+        }
+      })
+    })
+}
+
+module.exports = {
+  generate: _ => {
+    const gen = determinUpdateRequired()
+    if (gen) {
+      generateFile()
+    }
+    return gen
+  }
+}
